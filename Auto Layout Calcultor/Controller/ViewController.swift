f045037a17ea10ec59be19a520df27af0151d121
Hansa Anuradha <hansaanuradha@Hansas-MacBook-Pro.local>
2019-01-20 21:08:34 +0530

    layout, Conform to MVC

---
+++ Auto Layout Calcultor/Controller/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Auto Layout Calcultor
+//
+//  Created by Hansa Anuradha on 12/18/18.
+//  Copyright © 2018 Hansa Anuradha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    // MARK: - Properties
+
+    
+    // MARK: - IBOutlets
+
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    // MARK: - ViewController Methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    // MARK: - IBActions
+
+    @IBAction func numberButtonPressed(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func calculateButtonPressed(_ sender: UIButton) {
+    }
+    
+    
+    
+}
+
