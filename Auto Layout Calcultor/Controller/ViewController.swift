6fb20d31263be3a630dffa890040e910768be2c9
Hansa Anuradha <hansaanuradha@Hansas-MacBook-Pro.local>
2019-01-20 23:38:25 +0530

    Basic calculation actions

--- Auto Layout Calcultor/Controller/ViewController.swift
+++ Auto Layout Calcultor/Controller/ViewController.swift
@@ -40,8 +40,20 @@ class ViewController: UIViewController {
                 // User is not finished typing yet, so we set isFinishedTypingNumber property to false
                 isFinishedTypingNumber = false
             } else {
+                
+                guard let currentDisplayValue = Double(displayLabel.text!) else { fatalError("Can not convert display label value to Double") }
+                
+                // Checks display text is Integer or Non-Integer
+                let isInt = floor(currentDisplayValue) == currentDisplayValue
+                
+                // If current display text contains a Non Integer value, then stop this numberButtonPressed IBAction
+                if !isInt {
+                    return
+                }
+                
                 // Display the numbers on the display label
                 displayLabel.text?.append(numberValue)
+                
             }
         }
         
@@ -52,6 +64,24 @@ class ViewController: UIViewController {
         
         // User has finished typing numbers just now, so we set isFinishedTypingNumber property to true
         isFinishedTypingNumber = true
+        
+        // Convert whats in the display label to double value
+        guard let number = Double(displayLabel.text!) else {
+            fatalError("Can not convert display label text to Double")
+        }
+        
+        // Do the basic calculation functions
+        if let calculatorMethod = sender.currentTitle {
+            
+            if calculatorMethod == "AC" {
+                displayLabel.text = "0"
+            } else if calculatorMethod == "+/-" {
+                displayLabel.text = "\(number * -1)"
+            } else if calculatorMethod == "%" {
+                displayLabel.text = "\(number * 0.01)"
+            }
+        }
+        
     }
     
     
